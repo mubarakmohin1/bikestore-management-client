13bd6129c9f34edc04b4946a07287cab85745e1b
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-08 16:26:31 +0600

    updatae data

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -18,14 +18,16 @@ const Services = () => {
         navigate('/myitems')
     }
     return (
-        <div id='collection' className='row container mt-5 ms-5'>
+        <div>
 
-            <h1 className='text-center mb-5'>Stock collection</h1>
+           <div id='collection' className='row container mt-5 ms-5'>
+           <h1 className='text-center mb-5'>Stock collection</h1>
             {
                 stocks?.slice(0, 6).map(stock => <Stock key={stock._id} stock={stock}></Stock>)
             }
-            <Button className='w-50 mx-auto mt-5' onClick={() => handleManageInventory()}>Manage Inventory</Button>
         </div>
+        <Button className='w-50 mx-auto mt-5' onClick={() => handleManageInventory()}>Manage Inventory</Button>
+           </div>
     );
 };
 
