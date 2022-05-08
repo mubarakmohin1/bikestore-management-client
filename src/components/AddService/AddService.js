56dfc0899172c6d960a9c30202ad40f83d141117
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-08 07:56:47 +0600

    stock added successful

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -2,23 +2,38 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-   
-    return (
-       <div className='w-50 mx-auto'>
-            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-      <input className='mb-2 mt-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
-      <input className='mb-2 mt-2' placeholder='supplierName' {...register("suppliername", { required: true, maxLength: 20 })} />
-      <input  type="number" className='mb-2' placeholder='price' {...register("price")} />
-      <input  type="number" className='mb-2' placeholder='quantity' {...register("quantity")} />
-      <input className='mb-2' placeholder='about' {...register("about")} />
-      <input className='mb-2' placeholder='photo url' {...register("picture")} />
-      
-      <input type="Add Stock" />
-    </form>
-       </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+
+    console.log(data)
+    const url = `http://localhost:5000/stock`;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+      })
+  };
+
+  return (
+    <div className='w-50 mx-auto'>
+      <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+        <input className='mb-2 mt-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+        <input className='mb-2 mt-2' placeholder='supplierName' {...register("suppliername", { required: true, maxLength: 20 })} />
+        <input type="number" className='mb-2' placeholder='price' {...register("price")} />
+        <input type="number" className='mb-2' placeholder='quantity' {...register("quantity")} />
+        <input className='mb-2' placeholder='about' {...register("about")} />
+        <input className='mb-2' placeholder='photo url' {...register("picture")} />
+
+        <input type="submit" value="Add Stock" />
+      </form>
+    </div>
+  );
 };
 
 export default AddService;
